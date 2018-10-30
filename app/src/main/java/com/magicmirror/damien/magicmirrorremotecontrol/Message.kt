b583bb219a029eea0b505719897dce125ad890ba
Damien Lenoir <dlenoir@pageup.fr>
2018-10-30 11:32:06 +0100

    get user message

--- app/src/main/java/com/magicmirror/damien/magicmirrorremotecontrol/Message.kt
+++ app/src/main/java/com/magicmirror/damien/magicmirrorremotecontrol/Message.kt
@@ -2,11 +2,21 @@ package com.magicmirror.damien.magicmirrorremotecontrol
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_message.view.*
 
 class Message : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
+    }
+
+    fun sendMessage(view: View) {
+        val message = findViewById<EditText>(R.id.monMessage)
+        println("test print")
+        println(message.text)
     }
 }
